de42ecd77487832e16e5d04fb56a7d7beef02f62
Vian Charbonneau <vian_charbonneau@yahoo.com>
2022-04-03 21:58:27 -0600

    ok

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Test");
+//console.log("Test");
+window.onload = function () {
+    initShoppingList();
+};
+
+function initShoppingList() {
+    let form = document.getElementById("item-form")
+    console.log(form);
+
+    form.addEventListener("submit", (event) => {
+             handleItemForm(event, form);
+    });
+}
+function handleItemForm(event, formRef) {
+    if(event.preventDefault) {
+        event.preventDefault();
+    }
+    console.log("Event: ", event);
+
+    return false;
+}
